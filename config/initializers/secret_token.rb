328af3bd0d4da27431e744b890b512dfb0d3205b
Hari <dreamer@Hari.(none)>
2013-06-19 18:32:38 +0530

    Initial configuration of a rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineTicketing::Application.config.secret_token = '208ac8d83c8608839af7236508bb56f544705e4fcf1cf8fcdb7a1d41a3375da53746f2e957640426ebc8e4f6b2f3d623beff8053ea60c2fc8dd81e7b6277e1e0'
